0a1e884bdecb09ffa8242e77d4ee1b2d8e7af010
Gustavo Jodar <gustavo.jodar@gmail.com>
2023-02-17 11:46:02 +0100

    ha

--- main.c
+++ main.c
@@ -56,6 +56,6 @@ int main(void) {
     };
     
     
-    //cmocka_set_message_output(CM_OUTPUT_XML);
+    cmocka_set_message_output(CM_OUTPUT_XML);
     return cmocka_run_group_tests_name("toto", tests, NULL, NULL);
 }
